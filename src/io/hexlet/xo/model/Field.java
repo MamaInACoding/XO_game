e71f191d10fa8392e3d15d8d3a0ed5fcf018d7b7
Feruza Adilova <bakh2401@gmail.com>
2021-08-06 15:28:37 +0500

    changes in Field = setFigure

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,14 +1,13 @@
 package io.hexlet.xo.model;
 
 
-import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class Field {
     private final int FIELD_SIZE = 3;
-    public final int MIN_COORDINSTE = 0;
+    public final int MIN_COORDINATE = 0;
     private final Figure[][] figures = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getFIELD_SIZE() {
@@ -16,27 +15,25 @@ public class Field {
     }
 
     public Figure getFigure(Point point) throws InvalidPointException {
-        if (!checkPoint(point)) {
+        if (checkPoint(point)) {
             throw new InvalidPointException();
         }
         return figures[point.x][point.y];
     }
 
-    public void setFigure(Point point, Figure figure) throws InvalidPointException, AlreadyOccupiedException {
-        if (!checkPoint(point)){
+    public void setFigure(Point point, Figure figure) throws InvalidPointException {
+        if (checkPoint(point)){
             throw new InvalidPointException();
         }
-        if (figures[point.x][point.y] != null) {
-            throw new AlreadyOccupiedException();
-        }
+
         figures[point.x][point.y] = figure;
     }
 
     public boolean checkPoint(Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return !checkCoordinate(point.x) || !checkCoordinate(point.y);
     }
 
     public boolean checkCoordinate (final int coordinate) {
-        return coordinate >= MIN_COORDINSTE && coordinate < FIELD_SIZE;
+        return coordinate >= MIN_COORDINATE && coordinate < FIELD_SIZE;
     }
 }
