8a9121944bddc88a1023515b4c5199f3fc08035c
Feruza Adilova <bakh2401@gmail.com>
2021-08-11 15:07:29 +0500

    fixing ArraysBoundException

--- src/io/hexlet/xo/controllers/WinnerController.java
+++ src/io/hexlet/xo/controllers/WinnerController.java
@@ -1,5 +1,6 @@
 package io.hexlet.xo.controllers;
 
+
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
@@ -7,56 +8,56 @@ import io.hexlet.xo.model.exceptions.InvalidPointException;
 import java.awt.*;
 
 public class WinnerController {
-    public Figure getWinner(Field field) {
+
+    public Figure getWinner(final Field field) {
         try {
-            for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1))) {
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
                     return field.getFigure(new Point(i, 0));
-                }
 
-                for (i = 0; i < 3; i++) {
-                    if (check(field, new Point(i, 0), p -> new Point(p.x + 1, p.y ))) {
-                        return field.getFigure(new Point(0, i));
-                    }
+            for (int i = 0; i < 3; i++)
+                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
+                    return field.getFigure(new Point(0, i));
 
-                    if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1))) {
-                        return field.getFigure(new Point(0, 0));
-                    }
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                return field.getFigure(new Point(0, 0));
 
-                    if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1))) {
-                        return field.getFigure(new Point(0, 2));
-                    }
-                }
+            if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
+                return field.getFigure(new Point(1, 1));
 
-            }
-        } catch (InvalidPointException invalidPointException) {
-            invalidPointException.printStackTrace();
+        } catch (final InvalidPointException e) {
+            e.printStackTrace();
         }
-
         return null;
-}
+    }
 
-
-    public boolean check(Field field, final Point currentPoint, IPointGenerator pointGenerator ) {
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) {
         final Figure currentFigure;
         final Figure nextFigure;
         final Point nextPoint = pointGenerator.next(currentPoint);
-
         try {
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(nextPoint);
 
-        } catch (InvalidPointException e) {
-            return false;
+            if (currentFigure == null)
+                return false;
+
+            nextFigure = field.getFigure(nextPoint);
+        } catch (final InvalidPointException e) {
+            return true;
         }
-        if (currentFigure == null) return false;
-        if (currentFigure != nextFigure) return false;
+
+        if (currentFigure != nextFigure)
+            return false;
 
         return check(field, nextPoint, pointGenerator);
     }
 
-
     private interface IPointGenerator {
+
         Point next(final Point point);
+
     }
+
 }
